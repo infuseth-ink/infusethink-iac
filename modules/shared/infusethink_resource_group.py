34ed677201be97702d3fcfb81f616a8bf6666945
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-10-06 01:37:56 +0800

    ✏️ typo: fix resource group name typo

--- modules/shared/infusethink_resource_group.py
+++ modules/shared/infusethink_resource_group.py
@@ -18,7 +18,7 @@ class InfusethinkResourceGroup:
         """Create a Resource Group with simplified arguments."""
         return resources.ResourceGroup(
             f"rg-{name}-{environment}",
-            resource_group_name=f"rg-infuseth-{environment}",
+            resource_group_name=f"rg-infusethink-{environment}",
             location=location,
             tags={
                 "Environment": environment,
