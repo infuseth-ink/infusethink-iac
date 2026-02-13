8eda6e5533b475d8e35eb109cda92750fb957d52
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-02-14 00:52:38 +0800

    🔒️ security: add SSH key name afte rmanual import

--- __main__.py
+++ __main__.py
@@ -2,7 +2,9 @@ import pulumi
 
 from infusethink.backend.ec2 import AwsEc2Backend
 
-backend = AwsEc2Backend("infusethink-backend", instance_type="t3.micro")
+backend = AwsEc2Backend(
+    "infusethink-backend", ssh_key_name="infusethink-backend", instance_type="t3.micro"
+)
 
 pulumi.export("backend_instance_id", backend.instance_id)
 pulumi.export("backend_public_ip", backend.public_ip)
