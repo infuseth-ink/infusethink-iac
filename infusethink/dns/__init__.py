1666fd10fd1ab92887a5af748d65a241822d9ea5
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-02-14 04:22:09 +0800

    🧱 infra: create NS and A records for backend while preserving Namecheap Private Email

--- infusethink/dns/__init__.py
+++ infusethink/dns/__init__.py
@@ -1,3 +1,17 @@
+from .records import (
+    create_a_record,
+    create_dkim_record,
+    create_dmarc_record,
+    create_mx_records,
+    create_spf_record,
+)
 from .route53 import AwsRoute53Zone
 
-__all__ = ["AwsRoute53Zone"]
+__all__ = [
+    "AwsRoute53Zone",
+    "create_a_record",
+    "create_dkim_record",
+    "create_dmarc_record",
+    "create_mx_records",
+    "create_spf_record",
+]
