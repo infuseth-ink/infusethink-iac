9f7d96eae8b0d3dea00d7a3033391e18dded3e6a
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-04-24 22:59:52 +0800

    🎉 init: scaffold TF project skeleton without resources

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "EC2 instance ID of the backend server"
+  value       = null
+}
+
+output "public_ip" {
+  description = "Public IP address of the backend server"
+  value       = null
+}
+
+output "zone_id" {
+  description = "Route 53 hosted zone ID"
+  value       = null
+}
+
+output "name_servers" {
+  description = "Route 53 name servers for the hosted zone"
+  value       = null
+}
