121753384ae2bf5fb0252fbe1e6d036d9391b3c7
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-04-28 23:27:26 +0800

    ✨ feat: add utility output to get the entire staging DB URL

--- environments/shared/outputs.tf
+++ environments/shared/outputs.tf
@@ -33,3 +33,16 @@ output "db_master_secret_arn" {
   description = "ARN of the Secrets Manager secret holding the master credentials"
   value       = module.shared.db_master_secret_arn
 }
+
+output "staging_database_url" {
+  description = "Postgres connection URL for the staging logical DB (URL-encoded password)"
+  value = format(
+    "postgres://%s:%s@%s:%d/%s?sslmode=require",
+    module.shared.db_master_username,
+    urlencode(module.shared.db_master_password),
+    module.shared.db_address,
+    module.shared.db_port,
+    postgresql_database.infusethink_staging.name,
+  )
+  sensitive = true
+}
