95f126add057108849f95889fa45b71155043863
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-05-07 14:23:28 +0800

    🐛 fix: relax ARN since scoping to the correct repo is already done by OIDC

--- environments/shared/main.tf
+++ environments/shared/main.tf
@@ -270,7 +270,7 @@ resource "aws_iam_role_policy" "gha_deploy_frontend_amplify" {
         "amplify:ListJobs",
         "amplify:StopJob",
       ]
-      Resource = "arn:aws:amplify:${var.aws_region}:${data.aws_caller_identity.current.account_id}:apps/*/branches/*/deployments/*"
+      Resource = "*"
     }]
   })
 }
